f7fb501121ad1b8ca7074ba3a3d41ea0243f64ea
IsmaelPanier <acocapoto1@gmail.com>
2025-06-17 02:13:31 +0200

    Rajout : Separation de differente role

--- server/src/module/users/dto/create-user.dto.ts
+++ server/src/module/users/dto/create-user.dto.ts
@@ -1,4 +1,12 @@
-import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+  ValidateIf,
+} from 'class-validator';
 import { UserRole } from '../../../common/enums/user-role.enum';
 
 export class CreateUserDto {
@@ -13,26 +21,35 @@ export class CreateUserDto {
     @IsNotEmpty()
     name : string;
 
-
+    // ---- Champs communs aux utilisateurs classiques et livreurs
     @IsEnum(UserRole)
-    @IsOptional()
+    @IsOptional() // Peut être omis = prendra par défaut User coté schema
     role? : UserRole;
 
+ // ---- Champs communs aux utilisateurs classiques et livreurs
     @IsOptional()
     @IsString()
     adress?: string;
 
     @IsOptional()
     @IsString()
-    restaurantName?: string;
+    phone?: string;
 
-    @IsOptional()
-    @IsString()
-    restaurantDescription?: string;
+    // ---- Champs spécifiques aux restaurants ----
+  @ValidateIf((o) => o.role === UserRole.RESTAURANT)
+  @IsString()
+  @IsNotEmpty({ message: 'restaurantName est requis pour les restaurants' })
+  restaurantName?: string;
 
-    @IsOptional()
-    @IsString()
-    restaurantAdress?: string;
+  @ValidateIf((o) => o.role === UserRole.RESTAURANT)
+  @IsString()
+  @IsNotEmpty({ message: 'restaurantDescription est requis pour les restaurants' })
+  restaurantDescription?: string;
+
+  @ValidateIf((o) => o.role === UserRole.RESTAURANT)
+  @IsString()
+  @IsNotEmpty({ message: 'restaurantAdress est requis pour les restaurants' })
+  restaurantAdress?: string;
 
 
 
--- server/src/module/users/dto/update-user.dto.ts
+++ server/src/module/users/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
+// src/modules/users/dto/update-user.dto.ts
+
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto  } from './create-user.dto';
+import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
-
-// PartialType rend tout les champs optionnels automatiquement, ce qui est parfait pour les mise à jour (Patch / PUT)
